80c0c5de2ee28b716f62ef1110a8f4741bb30a10
hoquangbao1@gmail.com <hoquangbao1@gmail.com>
2020-12-19 15:39:00 +0700

    add create, update vocabulary

--- src/views/lessons-view/lessons-view.js
+++ src/views/lessons-view/lessons-view.js
@@ -16,7 +16,8 @@ const LessonsView = (props) => {
   const [conversationDataSource, setConverastionDataSource] = React.useState([])
   const [quizDataSource, setQuizDataSource] = React.useState([])
   const [isModalVisible, setModalVisible] = React.useState(false)
-  const [isVocabularyModalVisible, setVocabularyModalVisible] = React.useState(false)
+  const [isVocabularyUpdateModalVisible, setVocabularyUpdateModalVisible] = React.useState(false)
+  const [isVocabularyCreateModalVisible, setVocabularyCreateModalVisible] = React.useState(false)
   const [vocabularyModalContent, setVocabularyModalContent] = React.useState({})
   const columns = [
     {
@@ -86,11 +87,12 @@ const LessonsView = (props) => {
       render: (text, record) => {
         return (
           <Space size="middle">
-            <button type="primary" onClick={() => showVocabularyModal(record)}>Edit</button>
+            <button type="primary" onClick={() => showVocabularyUpdateModal(record)}>Update</button>
+            <button type="primary" onClick={() => showVocabularyCreateModal()}>Create</button>
           </Space>
         )
       }
-    }
+    },
   ]
   const conversationColumn = [
     {
@@ -134,9 +136,13 @@ const LessonsView = (props) => {
     // },
   ]
 
-  const showVocabularyModal = (record) => {
+  const showVocabularyUpdateModal = (record) => {
     setVocabularyModalContent(record)
-    setVocabularyModalVisible(true)
+    setVocabularyUpdateModalVisible(true)
+  }
+
+  const showVocabularyCreateModal = () => {
+    setVocabularyCreateModalVisible(true)
   }
 
   const showModal = (record) => {
@@ -222,7 +228,7 @@ const LessonsView = (props) => {
   }
 
   const handleSubmit = () => {
-    
+
   }
 
   useEffect(() => {
@@ -257,7 +263,74 @@ const LessonsView = (props) => {
   }, [])
 
   const onVocabularyFormFinish = values => {
-    console.log(values)
+    // async function updateVocabulary() {
+    //   try {
+    //     const result = await request.put(`/api/admin/updateVocabulary`, {
+    //       "description": vocabularyModalContent.description,
+    //       "id": vocabularyModalContent.id,
+    //       "image": vocabularyModalContent.image,
+    //       "listVocabulary": [
+    //         {
+    //           "id": vocabularyModalContent.listVocabulary[0].id,
+    //           "lessonID": vocabularyModalContent.listVocabulary[0].lessonID
+    //         }
+    //       ],
+    //       "vocabulary": vocabularyModalContent.vocabulary,
+    //       "voice_link": vocabularyModalContent.voice_link
+    //     })
+    //     if (result.code === 200) {
+    //       console.log('success')
+    //     } else {
+    //       message.error({
+    //         content: 'Something went wrong!',
+    //         style: {
+    //           position: 'fixed',
+    //           bottom: '10px',
+    //           left: '50%'
+    //         }
+    //       })
+    //     }
+    //   } catch (e) {
+    //     console.log(e)
+    //   }
+    // }
+    // updateVocabulary();
+  }
+
+  const onCreateVocabularyFormFinish = values => {
+    console.log("Create Submit")
+    // async function updateVocabulary() {
+    //   try {
+    // const result = await request.post(`/api/admin/addVocab/${vocabularyModalContent.listVocabulary[0].lessonID}`, {
+    //       "description": vocabularyModalContent.description,
+    //       "id": vocabularyModalContent.id,
+    //       "image": vocabularyModalContent.image,
+    //       "listVocabulary": [
+    //         {
+    //           "id": vocabularyModalContent.listVocabulary[0].id,
+    //           "lessonID": vocabularyModalContent.listVocabulary[0].lessonID
+    //         }
+    //       ],
+    //       "vocabulary": vocabularyModalContent.vocabulary,
+    //       "voice_link": vocabularyModalContent.voice_link
+    //     })
+    //     if (result.code === 200) {
+    //       console.log('success')
+    //     } else {
+    //       message.error({
+    //         content: 'Something went wrong!',
+    //         style: {
+    //           position: 'fixed',
+    //           bottom: '10px',
+    //           left: '50%'
+    //         }
+    //       })
+    //     }
+    //   } catch (e) {
+    //     console.log(e)
+    //   }
+    // }
+    // updateVocabulary();
   }
 
   return (
@@ -288,91 +361,172 @@ const LessonsView = (props) => {
                 setConverastionDataSource([])
               }}
             >
-                <div
-                  style={{ maxHeight: '60vh', overflowY: 'auto' }}
-                >
-                  <Tabs defaultActiveKey="tabVocabulary">
-                    <TabPane tab="Vocabulary" key="tabVocabulary">
-                      <Row justify="center">
-                        <Table
-                          dataSource={vocabularyDataSource}
-                          columns={vocabularyColumn}
-                          pagination={{
-                            position: ['bottomRight'],
-                            pageSize: 10
-                          }}
-                        />
-                        <Modal
-                          visible={isVocabularyModalVisible}
-                          width={900}
-                          onCancel={() => {
-                            setVocabularyModalVisible(false)
-                          }}
-                          footer={[
-                            <Button
-                              key="submit"
-                              form="vocaForm"
-                              default
-                              // loading={isModalLoading}
-                              htmlType="submit"
-                            >
-                              Submit
-                          </Button>
-                          ]}
-                        >
-                          <div
-                            style={{ maxHeight: '60vh', overflowY: 'auto' }}
+              <div
+                style={{ maxHeight: '60vh', overflowY: 'auto' }}
+              >
+                <Tabs defaultActiveKey="tabVocabulary">
+                  <TabPane tab="Vocabulary" key="tabVocabulary">
+                    <Row justify="center">
+                      <Table
+                        dataSource={vocabularyDataSource}
+                        columns={vocabularyColumn}
+                        pagination={{
+                          position: ['bottomRight'],
+                          pageSize: 10
+                        }}
+                      />
+                      <Modal
+                        visible={isVocabularyUpdateModalVisible}
+                        width={900}
+                        onCancel={() => {
+                          setVocabularyUpdateModalVisible(false)
+                        }}
+                        footer={[
+                          <Button
+                            key="submit"
+                            form="vocaForm"
+                            default
+                            // loading={isModalLoading}
+                            htmlType="submit"
                           >
-                            <Form
-                              id="vocaForm"
-                              name="vocaForm"
-                              initialValues={{ remember: true }}
-                              onFinish={onVocabularyFormFinish}
-                              onFinishFailed={(e) => console.log(e)}
+                            Submit
+                          </Button>
+                        ]}
+                      >
+                        <div
+                          style={{ maxHeight: '60vh', overflowY: 'auto' }}
+                        >
+                          <Form
+                            id="vocaForm"
+                            name="vocaForm"
+                            initialValues={{ remember: true }}
+                            onFinish={onVocabularyFormFinish}
+                            onFinishFailed={(e) => console.log(e)}
+                          >
+                            <h3>Vocabulary ID</h3> <Input value={vocabularyModalContent.id} disabled />
+                            <h3>Vocabulary</h3>
+                            <Form.Item
+                              name="vocabulary"
+                            // rules={[{ required: true, message: 'This field cannot be blank!' }]}
                             >
-                              <h3>Vocabulary ID</h3> <Input value={vocabularyModalContent.id} disabled />
-                              <h3>Vocabulary</h3>
-                                <Form.Item
-                                  name="vocabulary"
-                                  rules={[{ required: true, message: 'This field is required!' }]}
-                                >
-                                  <Input value={vocabularyModalContent.vocabulary}/>
-                                </Form.Item>
                               <Input value={vocabularyModalContent.vocabulary} />
-                              <h3>Vocabulary Description</h3> <Input value={vocabularyModalContent.description} />
-                              <h3>Vocabulary Image</h3> <Image src={vocabularyModalContent.image} width={300} height={300} />
-                              <h3>Vocabulary Voice</h3> <audio key={vocabularyModalContent.id} controls><source src={vocabularyModalContent.voice_link} type="audio/mpeg" /></audio>
-                            </Form>
-                            </div>
-                        </Modal>
-                      </Row>
-                    </TabPane>
-                    <TabPane tab="Conversation" key="tabConversation">
-                      <Row justify="center">
-                        <Table
-                          dataSource={conversationDataSource}
-                          columns={conversationColumn}
-                          pagination={{
-                            position: ['bottomRight'],
-                            pageSize: 10
-                          }}
-                        />
-                      </Row>
-                    </TabPane>
-                    <TabPane tab="Quiz" key="tabQuiz">
-                      <Row justify="center">
-                        <Table
-                          dataSource={quizDataSource}
-                          columns={quizColumn}
-                          pagination={{
-                            position: ['bottomRight'],
-                            pageSize: 10
-                          }}
-                        />
-                      </Row>
-                    </TabPane>
-                  </Tabs>
-                </div>
+                            </Form.Item>
+                            <h3>Vocabulary Description</h3>
+                            <Form.Item
+                              name="description"
+                            // rules={[{ required: true, message: 'This field cannot be blank!' }]}s
+                            >
+                              <Input placeholder={vocabularyModalContent.description} />
+                            </Form.Item>
+                            {/* <h3>Vocabulary Description</h3>
+                            <h3>Vocabulary Image</h3> <Image src={vocabularyModalContent.image} width={300} height={300} />
+                            <h3>Vocabulary Voice</h3> <audio key={vocabularyModalContent.id} controls><source src={vocabularyModalContent.voice_link} type="audio/mpeg" /></audio> */}
+                          </Form>
+                        </div>
+                      </Modal>
+                      {/* //update vocab */}
+                      <Modal
+                        title="Create Vocabulary"
+                        visible={isVocabularyCreateModalVisible}
+                        width={900}
+                        onCancel={() => {
+                          setVocabularyCreateModalVisible(false)
+                        }}
+                        footer={[
+                          <Button
+                            key="submitCreate"
+                            form="createVocabularyForm"
+                            default
+                            // loading={isModalLoading}
+                            htmlType="submit"
+                          >
+                            Submit
+                          </Button>
+                        ]}
+                      >
+                        <div
+                          style={{ maxHeight: '60vh', overflowY: 'auto' }}
+                        >
+                          <Form
+                            id="createVocabularyForm"
+                            name="createVocabularyForm"
+                            initialValues={{ remember: true }}
+                            onFinish={onCreateVocabularyFormFinish}
+                            onFinishFailed={(e) => console.log(e)}
+                          >
+                            <h3>Vocabulary ID</h3>
+                            <Form.Item
+                              name="vocabularyID"
+                            // rules={[{ required: true, message: 'This field cannot be blank!' }]}
+                            >
+                              <Input />
+                            </Form.Item>
+
+
+                            <h3>Vocabulary</h3>
+                            <Form.Item
+                              name="vocabulary"
+                            // rules={[{ required: true, message: 'This field cannot be blank!' }]}
+                            >
+                              <Input />
+                            </Form.Item>
+
+
+                            <h3>Vocabulary Description</h3>
+                            <Form.Item
+                              name="description"
+                            // rules={[{ required: true, message: 'This field cannot be blank!' }]}s
+                            >
+                              <Input />
+                            </Form.Item>
+
+                            <h3>Vocabulary Image</h3>
+                            <Form.Item
+                              name="description"
+                            // rules={[{ required: true, message: 'This field cannot be blank!' }]}s
+                            >
+                              <Input />
+                            </Form.Item>
+
+                            <h3>Vocabulary Voice</h3>
+                            <Form.Item
+                              name="description"
+                            // rules={[{ required: true, message: 'This field cannot be blank!' }]}s
+                            >
+                              <Input />
+                            </Form.Item>
+                          </Form>
+                        </div>
+                      </Modal>
+                      {/* //create vocab */}
+                    </Row>
+                  </TabPane>
+                  <TabPane tab="Conversation" key="tabConversation">
+                    <Row justify="center">
+                      <Table
+                        dataSource={conversationDataSource}
+                        columns={conversationColumn}
+                        pagination={{
+                          position: ['bottomRight'],
+                          pageSize: 10
+                        }}
+                      />
+                    </Row>
+                  </TabPane>
+                  <TabPane tab="Quiz" key="tabQuiz">
+                    <Row justify="center">
+                      <Table
+                        dataSource={quizDataSource}
+                        columns={quizColumn}
+                        pagination={{
+                          position: ['bottomRight'],
+                          pageSize: 10
+                        }}
+                      />
+                    </Row>
+                  </TabPane>
+                </Tabs>
+              </div>
             </Modal>
           </Row>
       }
